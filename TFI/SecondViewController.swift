4add14bddcfbd746413eff71eaed6016fdbd777f
Gabriel mechali <gabriel.mechali@gmail.com>
2017-01-23 20:50:58 -0800

    Initial Commit

---
+++ TFI/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TFI
+//
+//  Created by Gabriel mechali on 1/23/17.
+//  Copyright © 2017 Gabmech. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
